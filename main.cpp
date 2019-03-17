48ac099769e36d739b2b9da5fe672fb92a3241c9
Kristof <kristofosztopani@gmail.com>
2019-03-17 14:52:15 +0100

    arg setup

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    string path;
     cout<<"made by Osztopani Kristof"<<endl;
+    string path;
+    cout<<"argc: "<<argc<<endl;
+    if(argc==1){
     cout<<"Please give the path and the extension(.txt) : ";
     cin>>path;
+    }else{
+        path=argv[1];
+       cout<<"path: "<<path<<endl;
+    }
     Sodoku sodoku(path);
     sodoku.printArray();
     sodoku.solve();
